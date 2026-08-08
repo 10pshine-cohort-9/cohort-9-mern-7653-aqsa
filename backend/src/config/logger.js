4c09a2e82e5f37bb44d56cbd54efef709c48b070
astrophileAqsa123 <aqsaarif5678@gmail.com>
2026-08-08 12:41:57 +0500

    fix: configure pino pretty transport

--- backend/src/config/logger.js
+++ backend/src/config/logger.js
@@ -1,9 +1,11 @@
 import pino from "pino";
-const logger=pino({
-    target:'pino-pretty',
-    options:{
-        colorize:true,
-        translateTime:'SYS:standard',
-    }
-})
+const logger = pino({
+    transport: {
+        target: "pino-pretty",
+        options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+        },
+    },
+});
 export default logger;
